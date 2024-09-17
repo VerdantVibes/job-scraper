e49e1dfab6678077ea1c2904717821818db0a2ae
VerdantVibes <biletskyiivan423@gmail.com>
2024-09-18 00:45:47 +0200

    feat: main login

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import time
 
 driver = webdriver.Firefox()
-driver.get("https://www.indeed.com/")
+# driver.get("https://www.indeed.com/")
 
 input_job_name = driver.find_element_by_xpath('//*[@id="text-input-what"]')
 input_job_name.send_keys("data analyst")
@@ -28,3 +28,41 @@ df = pd.DataFrame(
         "Date": [""],
     }
 )
+
+while True:
+    # Imports the HTML of the current page into python
+    soup = BeautifulSoup(driver.page_source, "lxml")
+
+    # Grabs the HTML of each posting
+    postings = soup.find_all(
+        "div", class_="jobsearch-SerpJobCard unifiedRow row result clickcard"
+    )
+
+    # grabs all the details for each posting and adds it as a row to the dataframe
+    for post in postings:
+        link = post.find("a", class_="jobtitle turnstileLink").get("href")
+        link_full = "https://www.indeed.com/" + link
+        name = post.find("h2", class_="title").text.strip()
+        company = post.find("span", class_="company").text.strip()
+        try:
+            location = post.find(
+                "div", class_="location accessible-contrast-color-location"
+            ).text.strip()
+        except:
+            location = "N/A"
+        date = post.find("span", class_="date").text.strip()
+        try:
+            salary = post.find("span", class_="salaryText").text.strip()
+        except:
+            salary = "N/A"
+        df = df.append(
+            {
+                "Link": link_full,
+                "Job Title": name,
+                "Company": company,
+                "Location": location,
+                "Salary": salary,
+                "Date": date,
+            },
+            ignore_index=True,
+        )
